6295282f2afc3571ff5f9847af4d0aeabad087e3
Jhon Francis A. Duarte <94598599+jhonfrancisduarte@users.noreply.github.com>
2021-11-20 18:45:37 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import gsap from 'gsap'
+import { gsap } from "gsap"
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui'
